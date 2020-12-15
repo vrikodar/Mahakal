96621e02e3b86120f1945113d557c59e4e6d204b
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-15 00:54:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ echo [*] This Script will install all the required Dependencies....
 apt install python3-pip
 pip3 install termcolor
 pip3 install hashlib
+apt install hashid
 echo [*]All Dependencies installed Successfully...!
