b335cf9aeece34712af3c254156b733da213c660
SxNade <73271976+SxNade@users.noreply.github.com>
2020-12-15 00:27:31 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+ 
+echo [+] Mahakal By SxNade.......https://github.com/SxNade
+echo [*] This Script will install all the required Dependencies....
+apt install python3-pip
+pip3 install termcolor
+pip3 install hashlib
+echo [*]All Dependencies installed Successfully...!
